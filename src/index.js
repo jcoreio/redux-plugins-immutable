6dd3870e5d6aedc72d68711fceab31b14ccd0069
Andy Edwards <jedwards@fastmail.com>
2016-03-21 13:07:52 -0500

    fix pluginTypes export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as pluginActions from './lib/pluginActions';
 import pluginMiddleware from './lib/pluginMiddleware';
 import pluginReducer from './lib/pluginReducer';
-import pluginTypes from './lib/pluginTypes';
+import * as pluginTypes from './lib/pluginTypes';
 
 export {pluginActions, pluginMiddleware, pluginReducer, pluginTypes};
